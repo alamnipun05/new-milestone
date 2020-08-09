03398551c540f2a29e08033d7c5c603e69de9a9b
alamnipun05 <alamnipun05@gmail.com>
2020-08-09 10:14:54 -0700

    assignment

---
+++ assignment.js
@@ -0,0 +1,18 @@
+//feetToMile
+function feetToMile(feet) {
+    var mile = feet/5280;
+    return mile;
+    
+}
+var result = feetToMile(166666);
+console.log(result);
+
+//woodCalculator
+function woodCalculator() {
+    var tablecount = table * (feet)^3;
+    var chaircount = chair * (feet)^3;
+    return totalWood;
+    
+}
+var result = woodCalculator(table,chair);
+console.log(result);
